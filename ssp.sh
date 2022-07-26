0c42edecc53ed31f725154605444e2e28f94f873
Artem.Rufanov <arufanov@gmail.com>
2022-07-26 11:18:40 +0300

    change release

--- ssp.sh
+++ ssp.sh
@@ -5,5 +5,5 @@
 #git push --delete origin ssp-1.0.0
 
 # Handle mvn tasks
-atlas-release -DreleaseVersion=1.0.1 -DdevelopmentVersion=1.0.2-SNAPSHOT -Dtag=my-project-1.0.1 
+atlas-release -DreleaseVersion=1.0.0 -DdevelopmentVersion=1.0.1-SNAPSHOT -Dtag=ssp-1.0.0
 #atlas-release-rollback
